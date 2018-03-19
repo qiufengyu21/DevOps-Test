c1f01cd5c14455614ef18489e7aa1c3327a5cc22
patel-bhavik <bcpatel@ncsu.edu>
2018-03-19 15:58:16 -0400

    Test Prioritization

--- TestPrioritization/main.js
+++ TestPrioritization/main.js
@@ -2,41 +2,46 @@ var fs = require('fs'),
     xml2js = require('xml2js'),
     child  = require('child_process'); 
 var parser = new xml2js.Parser();
-var Bluebird = require('bluebird')
-
-var testReport =  '';//XML File path to report
+var Bluebird = require('bluebird');
+var reportDir =  '/Reports/';
+var fileName = '/TEST-com.github.stokito.unitTestExample.calculator.CalculatorTest.xml';
 
 calculatePriority();
 
-
-function readResults(result)
+function readResults(result,tests,isFirstTime)
 {
-    var tests = [];
-    console.log("Result: ",result);
     for( var i = 0; i < result.testsuite['$'].tests; i++ )
     {
-       tests.push({
-        name:   testcase['$'].name, 
-        time:   testcase['$'].time, 
-        status: testcase.hasOwnProperty('failure') ? "failed": "passed"
-        });
-    }    
-    return tests;
+        var testcase = result.testsuite.testcase[i];
+        if(isFirstTime){
+            test = {name:testcase['$'].name,time: parseFloat(testcase['$'].time),timePassed:0,timeFailed:0};
+            testcase.hasOwnProperty('failure') ? test.timeFailed++ : test.timePassed++;
+            tests.push(test);
+        }else{
+            testIndex = tests.findIndex((testObj => testObj.name == testcase['$'].name));
+            tests[testIndex].time = tests[testIndex].time + parseFloat(testcase['$'].time);
+            testcase.hasOwnProperty('failure') ? tests[testIndex].timeFailed++ : tests[testIndex].timePassed++;
+        }
+    }
+    return tests;    
 }
 
 async function calculatePriority()
 {
-    try{
-        child.execSync('cd simplecalc & mvn test');
-    }catch(e){}
-    var contents = fs.readFileSync(__dirname + testReport)
-    let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
-    var tests = readResults(xml2json);
+    var tests = [];
+    for(var i = 1; i <= 3; i++){
+        var contents = fs.readFileSync(__dirname + reportDir + i + fileName);
+        let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
+        var isFirstTime = false;
+        if(i == 1)
+            isFirstTime = true;
+        tests = readResults(xml2json,tests,isFirstTime);
+    }
+
+    tests.forEach( e => console.log(e));
     tests.sort(function(a,b){
-        if(a.status == b.status) return a.time - b.time;
-        if(a.status == "failed" && b.status == "passed") return 1;
-        if(b.status == "failed" && a.status == "passed") return 1;
+        
+        return  b["timeFailed"] - a["timeFailed"] || b["timePassed"] - a["timePassed"] || b["time"] - a["time"];
     });
-    
     tests.forEach( e => console.log(e));
 }
